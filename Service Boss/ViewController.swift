acedc9d7b123c693e01d9cc0a8e635b44ac29a6e
Boss <t.phitakgul@gmail.com>
2017-12-09 12:02:00 +0700

    init start app

---
+++ Service Boss/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Service Boss
+//
+//  Created by Apple on 12/9/2560 BE.
+//  Copyright © 2560 Boss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
